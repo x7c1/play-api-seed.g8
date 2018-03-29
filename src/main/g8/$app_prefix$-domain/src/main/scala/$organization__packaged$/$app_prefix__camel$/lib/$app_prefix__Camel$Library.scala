62c4bf2729e69f5d5693c4b564c6db5cd59dec9c
haco <t.aida@japantaxi.co.jp>
2018-03-29 15:52:46 +0900

    rename

--- src/main/g8/$app_prefix$-domain/src/main/scala/$organization__packaged$/$app_prefix__camel$/lib/$app_prefix__Camel$Library.scala
+++
@@ -1,6 +0,0 @@
-package $organization$.$app_prefix$.lib
-
-object $app_prefix;format="Camel"$Library {
-  def createMessageFor(name: String) = s"hello, \$name!"
-}
-
--- src/main/g8/$app_prefix$-play/src/main/scala/$organization__packaged$/$app_prefix__camel$/app/$name__Camel$.scala
+++ src/main/g8/$app_prefix$-play/src/main/scala/$organization__packaged$/$app_prefix__camel$/app/$name__Camel$.scala
@@ -1,9 +1,9 @@
 package $organization$.$app_prefix$.app
 
-import $organization$.$app_prefix$.lib.$app_prefix;format="Camel"$Library
+import $organization$.$app_prefix$.domain.Hello$app_prefix;format="Camel"$
 
 object $name;format="Camel"$ extends App {
-  val message = $app_prefix;format="Camel"$Library createMessageFor "$name;format="Camel"$"
+  val message = Hello$app_prefix;format="Camel"$ createMessageFor "$name;format="Camel"$"
   println(message)
 }
 
