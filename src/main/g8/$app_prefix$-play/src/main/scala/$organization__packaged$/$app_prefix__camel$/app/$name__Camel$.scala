34eb595c6c8610ea2f6449ffba82ec39b31a7268
haco <t.aida@japantaxi.co.jp>
2018-03-29 17:44:53 +0900

    remove needless files

--- src/main/g8/$app_prefix$-play/src/main/scala/$organization__packaged$/$app_prefix__camel$/app/$name__Camel$.scala
+++
@@ -1,9 +0,0 @@
-package $organization$.$app_prefix$.app
-
-import $organization$.$app_prefix$.domain.Greeting
-
-object $name;format="Camel"$ extends App {
-  val message = Greeting messageFor "$name;format="Camel"$"
-  println(message)
-}
-
