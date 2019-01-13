ae72a0286450b705c5486f01a5ad02ac12c40e00
x7c1 <jmtaro+x7c1@gmail.com>
2019-01-13 22:53:07 +0900

    add urls of mvnrepository.com

--- src/main/g8/project/$app_prefix__Camel$Settings.scala
+++ src/main/g8/project/$app_prefix__Camel$Settings.scala
@@ -4,6 +4,7 @@ import sbt.Def.SettingList
 
 object $app_prefix;format="Camel"$Settings {
   lazy val commons = new SettingList(Seq(
+    // https://mvnrepository.com/artifact/org.scala-lang/scala-library
     scalaVersion := "2.12.6",
     scalacOptions ++= Seq(
       "-deprecation",
@@ -12,6 +13,7 @@ object $app_prefix;format="Camel"$Settings {
       "-Xfuture",
     ),
     libraryDependencies ++= Seq(
+      // https://mvnrepository.com/artifact/org.scalatestplus.play/scalatestplus-play
       "org.scalatest" %% "scalatest" % "3.1.2" % Test,
     ),
   ))
--- src/main/g8/project/plugins.sbt
+++ src/main/g8/project/plugins.sbt
@@ -1,1 +1,2 @@
+// https://mvnrepository.com/artifact/com.typesafe.play/play
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.21")
