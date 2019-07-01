78dd8e9fedf6f5b90f7225d6f3a79f3c7ef219d9
dyoshimitsu <d.yoshimitsu@gmail.com>
2019-07-01 22:48:35 +0900

    update scala version

--- src/main/g8/project/$app_prefix__Camel$Settings.scala
+++ src/main/g8/project/$app_prefix__Camel$Settings.scala
@@ -5,7 +5,7 @@ import sbt.Def.SettingList
 object $app_prefix;format="Camel"$Settings {
   lazy val commons = new SettingList(Seq(
     // https://mvnrepository.com/artifact/org.scala-lang/scala-library
-    scalaVersion := "2.12.6",
+    scalaVersion := "2.13.0",
     scalacOptions ++= Seq(
       "-deprecation",
       "-feature",
